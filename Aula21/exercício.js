4b70e01ed2d43c288a37fea43246c54821708cb7
GraziMonteiro <monteirograziela385@gmail.com>
2021-10-27 22:08:00 -0300

    Exercício JavaScript

---
+++ Aula21/exercício.js
@@ -0,0 +1,12 @@
+function calculoIMC() {
+var nome = String (prompt("Digite seu nome"));
+var peso = Number (prompt("Digite seu peso"));
+var altura = Number (prompt("Digite seu altura"));
+var IMC 
+
+IMC = (peso/altura*altura);
+
+alert(`${nome} tem o peso de ${peso} e a altura ${altura}, então seu ${IMC}`);
+
+}
+
