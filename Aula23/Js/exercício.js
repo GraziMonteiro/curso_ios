4ae952004a3c5f44e66974d6c11f561e51f826b2
GraziMonteiro <monteirograziela385@gmail.com>
2021-11-04 18:38:08 -0300

    Atividade Arrays

---
+++ Aula23/Js/exercício.js
@@ -0,0 +1,10 @@
+// unshift()
+console.clear();
+const pessoas1 = ['Joaquim', 'José' , 'Silva', 'Xavier'];
+let w = pessoas1.unshift('Amanda'); // w = 5
+let x = pessoas1.pop(); // x = "Xavier"
+
+console.log(pessoas1);
+console.log(w);
+
+
