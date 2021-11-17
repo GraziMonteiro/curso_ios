f6b2b767f4a5467cbc70de994acc183815b2b0d1
GraziMonteiro <monteirograziela385@gmail.com>
2021-11-17 19:42:38 -0300

    Java

--- Aula28/exercício.js
+++ Aula28/exercício.js
@@ -11,23 +11,26 @@ class contaBancaria {
     }
 
     contaPoupança(){
-        return this.salario - (this.salario * 0.015) ;
+        return this.salario * 0.015;
     }
 
     contaCorrente(){
-        return this.salario - (this.salario * 0.036) ;
+        return this.salario * 0.036 ;
     }
 
     contaEstudante(){
-        return this.salario - (this.salario * 0.012) ;
+        return this.salario * 0.012 ;
     }
 
+    descontoConta(){
+        return this.salario - (this.salario  * 0.012);
+    }
 }
 
 let aluna = new contaBancaria('Isabela', '20', 1500, 'Feminino', 'Banco Pipoca','013', 'contaEstudante');
 
 console.log(aluna);
-console.log(`O Novo Salário é: `, aluna.contaEstudante());
+console.log(`O Novo Salário é: ${aluna.descontoConta()} reais`);
 
 
 
