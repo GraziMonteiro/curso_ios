ea1931683cc0093c21314f6e6a9b38b14275f9d6
GraziMonteiro <monteirograziela385@gmail.com>
2021-11-18 16:50:39 -0300

    Aprendendo Java

--- Aula29/Js/exercicio.js
+++ Aula29/Js/exercicio.js
@@ -10,17 +10,17 @@ function alertmes(){
         h3.style.borderBottom = 'solid 2px #000';
         h3.style.color = '#ffffff';
 
-    } else if (mes = 'Abril', mes = 'Maio', mes = 'Junho'){ //PRIMAVERA
+    } else if(mes = 'Abril', mes = 'Maio', mes = 'Junho'){ //PRIMAVERA
         h3.innerHTML = `O mês é ${mes}. Portanto a estação do ano é Inverno`;
         h3.style.background = '#49C526';
         h3.style.borderBottom = 'solid 2px #000';
 
-    }else if (mes = 'Julho', mes = 'Agosto', mes = 'Setembro'){ //VERÂO 
+    }else if(mes = 'Julho', mes = 'Agosto', mes = 'Setembro'){ //VERÂO 
         h3.innerHTML = `O mês é ${mes}. Portanto a estação do ano é Inverno`;
         h3.style.background = '#EDF108';
         h3.style.borderBottom = 'solid 2px #000';
 
-    }else if (mes = 'Outono', mes = 'Novembro', mes = 'Dezembro'){ // OUTUBRO
+    }else(mes = 'Outono', mes = 'Novembro', mes = 'Dezembro'){ // OUTUBRO
         h3.innerHTML = `O mês é ${mes}. Portanto a estação do ano é Inverno`;
         h3.style.background = '#A94E1D';
         h3.style.borderBottom = 'solid 2px #000';
