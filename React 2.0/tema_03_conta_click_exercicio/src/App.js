8263cabcc2478b9d1f076d2af91c74b29158191f
GraziMonteiro <monteirograziela385@gmail.com>
2021-11-27 21:15:42 -0300

    Atividade e Vamos Praticar

---
+++ React 2.0/tema_03_conta_click_exercicio/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';  
+import Exemplo from './Components/Exemplo';  
+import Header from './Components/Header';  
+  
+function App() {  
+    return (  
+        <div className="App">  
+            <Header nome="Morty" />    
+            <Exemplo />  
+	        </div>  
+	    );  
+	}  
+	  
+	export default App;  
+
